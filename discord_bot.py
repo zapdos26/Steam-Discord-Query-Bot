a50ceda09effa526d5f06fb9a8bc5920a0852295
Zapdos26 <zapdoskid@gmail.com>
2019-11-26 20:34:25 -0500

    Initial Commit

---
+++ discord_bot.py
@@ -0,0 +1,29 @@
+import os
+
+import discord
+from discord.ext import commands
+
+from utils import settings
+
+bot = commands.Bot(command_prefix='!', description="A bot to post/update a Steam Server server status in real time.",
+                   case_insensitive=True)
+config = settings.get('main.json')
+
+for file in os.listdir("cogs"):
+    if file.endswith(".py"):
+        name = file[:-3]
+        bot.load_extension(f"cogs.{name}")
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    print(discord.utils.oauth_url(bot.user.id))
+    game = discord.Game("ARK")
+    await bot.change_presence(activity=game)
+
+
+bot.run(config['TOKEN'])
